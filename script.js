6675eb4aebe33b18029472de79b3c223ab337f85
EmanueleManno <mannus1994@gmail.com>
2023-06-06 16:43:35 +0200

    Funzione palindroma

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 //PROVO A VEDERE SE FUNZIONA
 console.log('JS OK');
 
-// # PALINDROMA
+// # PALINDROMA #
 
 //CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA:
 const userWord = prompt('Inserisci una parola, ti dirò se è palindroma o no');
 console.log(userWord);
 
 //RIBALTO LA PAROLA INSERITA DALL'UTENTE:
-const reverseWord = userWord.split("").reverse().join("");
+const reverseWord = "";
 
-//VERIFICO SE LA PAROLA RIBALTATA E QUELLA INSERITA CORRISPONDONO:
-if (userWord == reverseWord) {
-  console.log(userWord + " è palindroma");
+// VERIFICO SE SONO UGUALI DICHIARANDO LA FUNZIONE:
+function palindroma(userWord, reverseWord) {
+
+    for (let i = userWord.length - 1; i >= 0; i--) {
+        reverseWord += userWord[i];
+}
+    if (reverseWord == userWord) {
+        console.log(userWord + " è palindroma");
+}
+    else {
+        console.log(userWord + " non è palindroma");
+}
 }
 
-else {
-  console.log(userWord + " non è palindroma");
-}
+//ESEGUO LA FUNZIONE:
+palindroma(userWord, reverseWord);
 
-// # PARI E DISPARI
+
+
+// # PARI E DISPARI #
 
 //CHIEDERE ALL'UTENTE SE VUOLE SCEGLIERE TRA PARI O DISPARI:
 const evenOrOdd = prompt("Scrivi pari o dispari");
@@ -29,23 +39,26 @@ console.log(evenOrOdd);
 const userNumber = parseInt(prompt("Scrivi un numero da 1 a 5"));
 console.log(userNumber);
 
-//RECUPERARE IL NUMERO DEL COMPUTER:
+//RECUPERARE IL NUMERO DEL COMPUTER ESEGUENDO LA FUNZIONE: 
 const cpuNumber = getRandomNumber(1,5);
 console.log(cpuNumber);
 
 //SOMMARE I DUE NUMERI:
 const sum = userNumber + cpuNumber;
 
-//FUNZIONE PER GENERARE UN NUMERO RANDOM DEL COMPUTER:
+//FUNZIONE PER GENERARE UN NUMERO RANDOM DEL COMPUTER DICHIARANDO LA FUNZIONE:
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// SE E' PARI (SE LO DIVIDO PER DUE MI DA RESTO ZERO):
 if (sum % 2 == 0) {
+  //ALLORA MI STAMPI CHE LA SOMMA E' PARI:
   console.log(sum + " è pari");
 }
+//ALTRIMENTI MI STAMPI CHE LA SOMMA E' DISPARI:
 else {
   console.log(sum + " è dispari");
 }
