7b43b4bcf47f142e53d280684edd061f2a1657f1
EmanueleManno <mannus1994@gmail.com>
2023-06-06 15:47:54 +0200

    Risoluzione esercizio pari e dispari

--- script.js
+++ script.js
@@ -18,3 +18,34 @@ if (userWord == reverseWord) {
 else {
   console.log(userWord + " non è palindroma");
 }
+
+// # PARI E DISPARI
+
+//CHIEDERE ALL'UTENTE SE VUOLE SCEGLIERE TRA PARI O DISPARI:
+const evenOrOdd = prompt("Scrivi pari o dispari");
+console.log(evenOrOdd);
+
+//CHIEDERE DI INSERIRE UN NUMERO DA 1 A 5:
+const userNumber = parseInt(prompt("Scrivi un numero da 1 a 5"));
+console.log(userNumber);
+
+//RECUPERARE IL NUMERO DEL COMPUTER:
+const cpuNumber = getRandomNumber(1,5);
+console.log(cpuNumber);
+
+//SOMMARE I DUE NUMERI:
+const sum = userNumber + cpuNumber;
+
+//FUNZIONE PER GENERARE UN NUMERO RANDOM DEL COMPUTER:
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+if (sum % 2 == 0) {
+  console.log(sum + " è pari");
+}
+else {
+  console.log(sum + " è dispari");
+}
