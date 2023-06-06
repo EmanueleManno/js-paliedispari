926438b7bee6b2cfe1c8cc329091d72974bc6efe
EmanueleManno <mannus1994@gmail.com>
2023-06-06 15:35:31 +0200

    Secondo tentativo di risoluzione parola palindroma

--- script.js
+++ script.js
@@ -7,17 +7,14 @@ console.log('JS OK');
 const userWord = prompt('Inserisci una parola, ti dirò se è palindroma o no');
 console.log(userWord);
 
-//DIVIDO LA PAROLA IN PIU' STRINGHE CREANDO UN ARRAY CON LE LETTERE DELLA PAROLA:
-const lettersUserWord = userWord.split('');
-console.log(lettersUserWord);
+//RIBALTO LA PAROLA INSERITA DALL'UTENTE:
+const reverseWord = userWord.split("").reverse().join("");
 
-//RIBALTO LE STRINGHE DELLA PAROLA GENERANDO UN NUOVO ARRAY CON LE LETTERE DELLA PAROLA AL CONTRARIO:
-const reverseUserWord = lettersUserWord.reverse();
-console.log(reverseUserWord);
+//VERIFICO SE LA PAROLA RIBALTATA E QUELLA INSERITA CORRISPONDONO:
+if (userWord == reverseWord) {
+  console.log(userWord + " è palindroma");
+}
 
-//CONTROLLO SE LA PAROLA AL CONTRARIO CORRISPONDE CON LA PAROLA ORIGINALE:
-if (lettersUserWord == reverseUserWord) {
-    console.log('La parola è palindroma!');
-} else if (lettersUserWord != reverseUserWord) {
-    console.log('La parola non è palindroma!');
+else {
+  console.log(userWord + " non è palindroma");
 }
